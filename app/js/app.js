aa541a4970ffa1a74c673d06eb7c429b5c6a1a9c
Maeckeul <cmaeckeul@gmail.com>
2020-09-10 19:17:30 +0200

    :wrench: Carousel on the road

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,41 @@
-var app = {
-  init: function() {
-    console.log('init');
-  }
-};
+const carouselSlide = document.querySelector('.main__films--list');
+const carouselImages = document.querySelectorAll('.main__films--item');
 
-$(app.init);
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+console.log(size);
+
+carouselSlide.style.transform = 'translateX(' + (-200 * counter) + 'px)';
+
+nextBtn.addEventListener('click', ()=> {
+  if(counter >= carouselImages.length -1) return;
+  console.log(carouselImages.length)
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  console.log(counter);
+  carouselSlide.style.transform = 'translateX(' + (-200 * counter) + 'px)';
+})
+
+prevBtn.addEventListener('click', ()=> {
+  if(counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-200* counter) + 'px)';
+})
+
+carouselSlide.addEventListener('transitionend', () => {
+  if(carouselImages[counter].id === 'lastClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-200 * counter) + 'px)';
+  }
+
+  if(carouselImages[counter].id === 'firstClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-200 * counter) + 'px)';
+  }
+});
